91fc2992f81e1f6ee862a72bb85e5ce848993789
Jim Griesmer <jimgpub@live.com>
2021-12-01 12:53:54 -0500

    Day 1 Part 2

--- advent-of-code/2021/Day 1.py
+++ advent-of-code/2021/Day 1.py
@@ -16,16 +16,45 @@ depths_len = len(depths)
 increases = 0
 decreases = 0
 
-for i, val in enumerate(depths, start=1):
-    if (i < depths_len):
-        print(str(depths[i-1]) + ', ' + str(depths[i]))
-        if (depths[i] != depths[i-1]):
-            if (depths[i] > depths[i-1]):
-                increases = increases + 1
-            else:
-                decreases = decreases + 1
+def count_inc_dec(a):
+    incs = 0
+    decs = 0
+    a_len = len(a)
+    for i, val in enumerate(a, start=1):
+        if (i < a_len):
+            print(str(a[i-1]) + ', ' + str(a[i]))
+            if (a[i] != a[i-1]):
+                if (a[i] > a[i-1]):
+                    incs = incs+ 1
+                else:
+                    decs = decs + 1
+    return incs, decs
+
+increases, decreases = count_inc_dec(depths)
 
 print('Increases: ' + str(increases))
 print('Decreases: ' + str(decreases))
         
+# %%
+# ## Part II
+# 3 day window
+
+# %%
+# Make new dataset for creating 3 day groups
+window = 3
+depths_sums = []
+for i, val in enumerate(depths, start = window):
+    if (i <= depths_len):
+        depths_sums.append(sum(depths[i-window:i]))
+
+# %%
+increases = 0
+decreases = 0
+
+increases, decreases = count_inc_dec(depths_sums)
+
+print('Increases: ' + str(increases))
+print('Decreases: ' + str(decreases))
+
+
 # %%
