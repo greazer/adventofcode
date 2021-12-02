52daeda2cd23fa0d978f68a491ce77209a74914d
Jim Griesmer <jimgpub@live.com>
2021-12-02 13:53:43 -0500

    Day 2 Part 2

--- advent-of-code/2021/Day 2.py
+++ advent-of-code/2021/Day 2.py
@@ -13,13 +13,18 @@ import pandas as pd
 # %%
 dirs_df = pd.read_csv('Day 2 Input.txt', delimiter=' ')
 
-
-# %%
-forward_progress = sum(dirs_df[(dirs_df.direction=='forward')].value)
-ups = sum(dirs_df[(dirs_df.direction=='up')].value)
-downs = sum(dirs_df[(dirs_df.direction=='down')].value)
-
-final = forward_progress * (downs - ups)
-        
-
+forward_progress = 0
+aim = 0
+depth = 0
+for index, row in dirs_df.iterrows():
+    if (row.direction == 'up'):
+        aim -= row.value
+    elif (row.direction == 'down'):
+        aim += row.value
+    else:
+        forward_progress += row.value
+        depth += aim*row.value
+    
+print('forward_progress=' + str(forward_progress) + ' ' + 'depth=' + str(depth))
+print('answer=' + str(forward_progress * depth))
 # %%
